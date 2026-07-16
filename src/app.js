26ad51a19ff9450c67dfae7bccf2b4f16eb414f1
m224t156-web <m224t156@st.kyoto-seika.ac.jp>
2026-07-16 17:45:16 +0900

    app

--- src/app.js
+++ src/app.js
@@ -1,4 +1,35 @@
 import './style.css';
+//ルフィ
+const luffyImg = document.getElementById('luffy-img');
+luffyImg.addEventListener('click', () => {
+  window.anime({
+    targets: luffyImg,
+    scaleY: [
+      { value: 1.0, duration: 0 },
+      { value: 2.0, duration: 100, easing: 'easeOutQuad' }, 
+      { value: 1.0, duration: 400, easing: 'easeOutElastic(1, .5)' } 
+    ],
+    scaleX: [
+      { value: 1.0, duration: 0 },
+      { value: 0.5, duration: 100, easing: 'easeOutQuad' },
+      { value: 1.0, duration: 400, easing: 'easeOutElastic(1, .5)' }
+    ],
+    easing: 'easeOutCubic',
+  });
+});
 
+//ゾロ
+const zoroImg = document.getElementById('zoro-img');
 
-// aminejsを使用して画像をクリックしたら大きくなったり動きをつけたりする
+zoroImg.addEventListener('click', () => {
+  window.anime({
+    targets: zoroImg,
+    rotate: '+=3turn', 
+    scale: [
+      { value: 0.7, duration: 200, easing: 'easeOutQuad' }, 
+      { value: 1.1, duration: 300, easing: 'easeOutQuad' }, 
+      { value: 1.0, duration: 300, easing: 'easeOutElastic(1, .6)' } 
+    ],
+    easing: 'easeOutCubic',
+  });
+});
